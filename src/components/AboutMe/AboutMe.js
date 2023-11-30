10ef82d8035b805837d792ef97a07ef70c4d1b47
Fox Alice <elviratr@yandex.ru>
2023-11-30 10:47:30 +0300

    feat: about me text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,8 +10,7 @@ function AboutMe() {
                 <div className='about-me__content-container'>
                     <h3 className="about-me__subtitle">Эльвира</h3>
                     <p className="about-me__description">Фронтенд-разработчик</p>
-                    <p className="about-me__content content">Я живу в Костроме, закончила институт экономики, управления и финансов КГТУ по специальности "финансы и кредит".
-                     У меня есть муж и сын. Я люблю играть в квизы, а ещё увлекаюсь волейболом. Недавно начала кодить. В 2023 году закончила курс "веб-разработчик" от Яндекс Практикума</p>
+                    <p className="about-me__content content">Меня привлекает веб-разработка, потому что я люблю решать сложные задачи, изучать новое, разбираться в коде и искать закономерности. Мне нравится видеть результат своей работы, которую совсем недавно я считала магией.</p>
                     <a className="about-me__github-link" href="https://github.com/Fox-Alice" target="_blank" rel="noreferrer">Github</a>
                 </div>
             </div>
